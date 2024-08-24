f88bb9a28d94104c21f93710afbb2ed846b532f5
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2024-08-24 14:46:04 +0200

    Use warnings stdlib in _deprecations #30
    Also properly wrap the deprecated function using functools.wrap

--- vbeam/util/_deprecations.py
+++ vbeam/util/_deprecations.py
@@ -1,4 +1,5 @@
-from typing import TypeVar
+import functools
+import warnings
 
 
 def _get_function_name(f: callable):
@@ -6,15 +7,18 @@ def _get_function_name(f: callable):
 
 
 def renamed_kwargs(version: str, **renamed_kwargs: str):
-    """Print a deprecation warning when passing an argument by name when that argument
-    has been renamed in a recent version.
+    """Warn when passing an argument by name when that argument has been renamed in a
+    recent version and the old name is deprecated.
 
+    >>> import warnings
     >>> @renamed_kwargs("1.0.5", b="renamed_arg")
     ... def f(a, renamed_arg):
     ...   return a + renamed_arg
-    >>> f(a=1, b=2)
-    Deprecation warning: argument 'b' of f was renamed to 'renamed_arg' in version 1.0.5.
+    >>> with warnings.catch_warnings(record=True) as w:
+    ...   f(a=1, b=2)
+    ...   print(w[0].message)
     3
+    Deprecation warning: argument 'b' of f was renamed to 'renamed_arg' in version 1.0.5.
 
     Using the new name or given positional arguments doesn't print the warning.
     >>> f(a=1, renamed_arg=2)
@@ -24,20 +28,23 @@ def renamed_kwargs(version: str, **renamed_kwargs: str):
     """
 
     def decorator(f):
-        def wrapped(*args, **kwargs):
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
             new_kwargs = {}
             for k, v in kwargs.items():
                 if k in renamed_kwargs:
-                    print(
+                    warnings.warn(
                         f"Deprecation warning: argument '{k}' of "
                         f"{_get_function_name(f)} was renamed to '{renamed_kwargs[k]}' "
-                        f"in version {version}."
+                        f"in version {version}.",
+                        DeprecationWarning,
+                        stacklevel=2,
                     )
                     k = renamed_kwargs[k]
                 new_kwargs[k] = v
             return f(*args, **new_kwargs)
 
-        return wrapped
+        return wrapper
 
     return decorator
 
